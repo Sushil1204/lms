789527b778cba9be20ada20f3a35a6796019e4f7
Sushil1204 <sushilspundkar@gmail.com>
2023-04-01 16:29:09 +0530

    members page created

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ mysql = MySQL(app)
 def index():
     return render_template('index.html')
 
+@app.route("/members")
+def members():
+    return render_template('members.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
