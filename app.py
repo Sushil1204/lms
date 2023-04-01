297f5b9c41157793c693becbb50fad07f99bda76
Sushil1204 <sushilspundkar@gmail.com>
2023-04-01 18:31:03 +0530

    feature created add members

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, url_for, redirect, request
+import requests 
 from flask_mysqldb import MySQL
 import MySQLdb
+from wtforms import Form, validators, StringField, FloatField, IntegerField, DateField, SelectField
 
 
 
@@ -11,7 +13,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Sushil@1204'
 app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_DB'] = 'librarydb'
+app.config['MYSQL_DB'] = 'library'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Initialise MYSQL
@@ -24,7 +26,51 @@ def index():
 
 @app.route("/members")
 def members():
-    return render_template('members.html')
+    # Create MySQLCursor
+    cur = mysql.connection.cursor()
 
+    # Execute SQL Query
+    result = cur.execute("SELECT * FROM members")
+    members = cur.fetchall()
+
+    # Render Template
+    if result > 0:
+        return render_template('members.html', members=members)
+    else:
+        msg = 'No Members Found'
+        return render_template('members.html', warning=msg)
+
+    # Close DB Connection
+    cur.close()
+
+class MemberForm(Form):
+    name = StringField('Name', [validators.length(min=3, max=50)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+
+@app.route("/add-member", methods=['GET', 'POST'])
+def add_member():
+    # Get form data from request
+    form = MemberForm(request.form)
+
+     # To handle POST request to route
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+
+         # Create MySQLCursor
+        cur = mysql.connection.cursor()
+
+        # Execute SQL Query
+        cur.execute(
+            "INSERT INTO members (name, email) VALUES (%s, %s)", (name, email))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close DB Connection
+        cur.close()
+        
+        return redirect(url_for('members'))    
+    return render_template('addMember.html', form=form)
 if __name__ == '__main__':
   app.run(debug=True)
