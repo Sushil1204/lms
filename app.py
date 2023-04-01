a73880d72b481141480fdbcd3c6e86084c1392ca
Sushil1204 <sushilspundkar@gmail.com>
2023-04-01 16:00:08 +0530

    base layout created

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import MySQLdb
+
 
 
 app = Flask(__name__)
+
+#mysql config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Sushil@1204'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_DB'] = 'librarydb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Initialise MYSQL
+mysql = MySQL(app)
 
 
 @app.route("/")
